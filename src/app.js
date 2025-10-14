23e4e8de9dbad1ec58c126858a0fd81861f9be98
Eiler Gomez <gomezeiler250@gmail.com>
2025-10-14 01:51:35 -0600

    first changes

--- src/app.js
+++ src/app.js
@@ -10,7 +10,11 @@ import ingresoRouter from "./routes/ingreso_producto.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
 
